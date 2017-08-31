e04717415a3656e3950de0a5d3000a5992f022b8
StefanU <stefanpu13@gmail.com>
2017-08-31 18:15:49 +0300

    Add solution to problem 17

---
+++ 017NumberLetterCounts.fsx
@@ -0,0 +1,103 @@
+(*
+    If the numbers 1 to 5 are written out in words: 
+    one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+    If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, 
+    how many letters would be used?
+
+
+    NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) 
+    contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. 
+    The use of "and" when writing out numbers is in compliance with British usage.
+*)
+
+(*
+1
+..
+10 - ten
+..
+20 - twenty
+21 - twenty one
+..
+30 - thirty
+31 - thirty one
+..
+100 - one hundred
+101 - one hundred and one
+..
+199 - one hundred and 99
+
+901
+
+1000 - one thousand
+
+Idea - 
+number % 100 +
+
+456 / 100
+*)
+
+let digitNames = function 
+    | 1 -> "one"
+    | 2 -> "two"
+    | 3 -> "three"
+    | 4 -> "four"
+    | 5 -> "five"
+    | 6 -> "six"
+    | 7 -> "seven"
+    | 8 -> "eight"
+    | 9 -> "nine"
+    |_ -> ""
+let tenToNineteenNames = function
+    | 10 -> "ten"
+    | 11 -> "eleven"
+    | 12 -> "twelve"
+    | 13 -> "thirteen"
+    | 14 -> "fourteen"
+    | 15 -> "fivteen"
+    | 16 -> "sixteen"
+    | 17 -> "seventeen"
+    | 18 -> "eighteen"
+    | 19 -> "nineteen"
+    |_ -> ""
+let tensNames = function
+    | 2 -> "twenty"
+    | 3 -> "thirty"
+    | 4 -> "forty"
+    | 5 -> "fifty"
+    | 6 -> "sixty"
+    | 7 -> "seventy"
+    | 8 -> "eighty"
+    | 9 -> "ninety"
+    | _ -> "" 
+    
+let hundred = "hundred"
+
+let rec numberToString (res: string) acountedForHundres n  = 
+    let andStr = if acountedForHundres then " " else " and " 
+    match n with
+    | thousands when n = 1000 -> "one thousand"
+    | hundreds when n >= 100 -> 
+        let currentRes = digitNames (hundreds / 100)
+        numberToString (currentRes+ " " + "hundred") false (hundreds % 100)
+    | aboveTwenty when n >= 20 ->        
+        let currentRes = tensNames (aboveTwenty / 10)
+        numberToString (res + andStr + currentRes) true (aboveTwenty % 10)
+    | tenToNinteen when n >=10 ->
+        let currentRes = tenToNineteenNames tenToNinteen        
+        res + andStr + currentRes
+    | onerToNine when n >= 1 ->
+       let currentRes = digitNames onerToNine
+       res + andStr + currentRes
+    | zero -> res
+
+numberToString "" true 1000 
+
+let removeWhiteSpaces (s: string) = s.Replace(" ", "") 
+let concat l r = sprintf "%s%s" l r
+
+[1..1000] 
+|> List.map (numberToString "" true) 
+|> List.reduce (+) 
+|> removeWhiteSpaces 
+|> String.length
