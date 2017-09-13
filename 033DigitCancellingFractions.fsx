4b5c0ba7c643165221b90c998f1ced8ed7a06ae9
StefanU <stefanpu13@gmail.com>
2017-09-13 19:18:48 +0300

    Add solution to 33

--- 033DigitCancellingFractions.fsx
+++ 033DigitCancellingFractions.fsx
@@ -12,21 +12,21 @@
     find the value of the denominator.
 *)
 
+let getDigits n = [|(n/10.0); (n % 10.0)|] |> Array.map (int >> float)
+let rec gcd x y = if y = 0.0 then x else gcd y (x % y)
+
 seq {
     for y in 10.0..99.0 do
     for x in 10.0..99.0 do
-        if x < y
-        then yield (x/y, string x,string y) 
+        if x < y then yield ( x/y, getDigits x, getDigits y )
 }
-// |> Array.ofSeq |> Seq.iter (printfn "%A")
-|> Seq.filter(fun (res,x,y ) ->
-    ((string>>float) x.[0] / (string>>float) y.[1] = res && x.[1] = y.[0]) ||
-    ((string>>float) x.[1] / (string>>float) y.[0] = res && x.[0]= y.[1])
-)
-
-
-string 48.0
-    ((string>>float) "49".[0] / (string>>float) "98".[1] = 0.5 && "49".[1] = "98".[0]) ||
-    ((string>>float) "49".[1] / (string>>float) "98".[0] = 0.5 && "49".[0]= "98".[1])
-
-float '4'
+|> Seq.map(fun (res,x,y ) ->
+    if (x.[0] / y.[1] = res && x.[1] = y.[0])
+    then (x.[0], y.[1])
+    elif (x.[1] / y.[0] = res && x.[0]= y.[1])
+    then (x.[1], y.[0])
+    else (1.0,1.0)
+) 
+|> Seq.filter (fun (x, y) -> x <> 1.0 || y <> 1.0) 
+|> Seq.reduce (fun ( numP, denomP) ( num, denom) -> numP * num, denomP * denom)
+|> (fun (num, denum) -> denum/ (gcd num denum))
