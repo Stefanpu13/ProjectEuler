4dd15c640e24d30d8003f6bb4da1badd085b0768
StefanU <stefanpu13@gmail.com>
2017-09-13 19:25:26 +0300

    Add example to 24

--- 024LexicographicPermutations.fsx
+++ 024LexicographicPermutations.fsx
@@ -5,6 +5,17 @@
     find i:  i * (n-1)! + (n-1)! > searchedCombIndex
     remaning = allBut i
     recursively repeat for remaining elems 
+
+    Example:
+    find 17th permutation of [0,1,2,3]
+
+    for 0 with index = 0 ->  1 * (4-1)! < 17 => 0 is not searhed number
+    for 1 with index = 1 -> 2 * (4-1)! < 17 ....
+    
+    for 2 with index = 2 -> 3 * (4-1)! > 17 => 2 is searched number and it index is 2
+
+    put 2 in permutation list and get remaining elements in order
+    for remaining elementsdo the same
 *)
 
 let fact n = List.reduce (*) [1..n]
@@ -22,11 +33,11 @@ let getPermutationAt elems permutationIndex =
                 )
 
             let remainingElems = 
-                 Array.concat [
-                    Array.take nextElemIndex elems 
-                    Array.skip (nextElemIndex + 1) elems
-                 ]
-            let permsCount = permutationIndex - (nextElemIndex) * remainingElemsPermutationsCount
+                Array.concat [
+                   Array.take nextElemIndex elems 
+                   Array.skip (nextElemIndex + 1) elems
+                ]
+            let permsCount = permutationIndex - nextElemIndex * remainingElemsPermutationsCount
 
             getPermutationAt permsCount (elems.[nextElemIndex]::foundMembers) remainingElems    
 
@@ -36,3 +47,5 @@ let getPermutationAt elems permutationIndex =
 getPermutationAt [|0..9|] 1000000 
 |> List.map string 
 |> List.reduce (+)
+
+getPermutationAt [|0..3|] 17
